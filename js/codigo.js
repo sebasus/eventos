35dd178d1f884ac3fd934e6f0b8eec27d44b522a
sebasus <susperregui.s@hotmail.com>
2022-08-08 23:05:09 -0300

    eventos

--- js/codigo.js
+++ js/codigo.js
@@ -1,7 +1,25 @@
 
 
-let casilla = document.querySelector(".casilla")
+let casilla = document.querySelectorAll(".casilla")
 
-console.log(casilla)
+let casillero = 
+
+casilla.forEach(function(casillero){
+    casillero.addEventListener ('click', function (e) {
 
 
+
+        casillero.style.backgroundColor = 'green'
+     })
+})
+
+
+
+casilla.forEach(function(casillero){
+    casillero.addEventListener ('dblclick', function (e) {
+
+
+
+        casillero.style.backgroundColor = 'red'
+     })
+})
